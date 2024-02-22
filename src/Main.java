02bd690f54e40f5dc7d247187e718fe1778953a4
Alisa <bilgeir@gmail.com>
2024-02-22 19:19:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 12892;
+        int mileCost = 27;
+        int mile = ticketCost / mileCost;
+        
+        System.out.println(mile);
+    }
+}
